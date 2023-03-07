5878494de05650a89159c5cb24cda3cfaee56e7c
zer0231 <pranay_zer0@protonmail.com>
2023-03-07 17:58:38 +0545

    created card view add_note + model

---
+++ app/src/main/java/com/zero/todoapp/database/NoteDao.kt
@@ -0,0 +1,20 @@
+package com.zero.todoapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.zero.todoapp.models.Note
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.ABORT )
+    suspend fun insert(note: Note)
+
+    @Delete()
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes_table ORDER BY id ASC")
+    fun getAllNotes():LiveData<List<Note>>
+
+    @Query("UPDATE notes_table SET title = :title, body= :body WHERE id= :id")
+    suspend fun update(id:Int?,title:String?,body:String?)
+}
