c3b57bf1d98473bfd8d755b4ac0ae49e08a4724f
zer0231 <pranay_zer0@protonmail.com>
2023-03-09 10:51:59 +0545

    fixed long press and added serializable in model

--- app/src/main/java/com/zero/todoapp/models/Note.kt
+++ app/src/main/java/com/zero/todoapp/models/Note.kt
@@ -6,8 +6,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "notes_table")
 data class Note(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int?,
     @ColumnInfo(name = "title") val title: String?,
     @ColumnInfo(name = "body") val body: String?,
     @ColumnInfo(name = "date") val date: String?
-)
+): java.io.Serializable
